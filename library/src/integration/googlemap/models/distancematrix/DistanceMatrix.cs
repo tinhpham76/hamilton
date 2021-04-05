9f9ada1cf29d5d516ae68e932b1a8113157c6148
tinhpham7676 <tinh_pham@outlook.com>
2021-04-06 01:14:52 +0700

    fix model libs google map api

--- library/src/integration/googlemap/models/distancematrix/DistanceMatrix.cs
+++ library/src/integration/googlemap/models/distancematrix/DistanceMatrix.cs
@@ -8,5 +8,6 @@ namespace Core.Libs.Integration.GoogleMap.Models.DistanceMatrix
         public string[] origin_addresses { get; set; }
         public List<Row> rows { get; set; }
         public string status { get; set; }
+        public string error_message { get; set; }
     }
 }
