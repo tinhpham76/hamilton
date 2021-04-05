bf600528c4b9f0068fef74b94e5afa0ccdbf8820
tinhpham7676 <tinh_pham@outlook.com>
2021-04-05 19:04:23 +0700

    add code

---
+++ backend/src/mapper/AutoMapperJsonModelProfile.cs
@@ -0,0 +1,11 @@
+using AutoMapper;
+
+namespace Hamiltion.Backend.Mapper
+{
+    public class AutoMapperJsonModelProfile : Profile
+    {
+        public AutoMapperJsonModelProfile()
+        {
+        }
+    }
+}
