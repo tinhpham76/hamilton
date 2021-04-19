a574a09341a88125340a99dc61dd43cea74d36d4
tinhpham7676 <tinh_pham@outlook.com>
2021-04-19 10:50:51 +0700

    fix

--- backend/src/business/HamiltonBusiness.cs
+++ backend/src/business/HamiltonBusiness.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Core.Libs.Integration.GoogleMap;
 
@@ -6,7 +7,7 @@ namespace Hamilton.Business
 {
     public interface IHamiltonBusiness
     {
-        Task<List<int[]>> Check(
+        Task<List<string[]>> Check(
             List<string> locations,
             long range);
     }
@@ -20,15 +21,22 @@ namespace Hamilton.Business
             this.client = client;
         }
 
-        public async Task<List<int[]>> Check(
+        public async Task<List<string[]>> Check(
             List<string> locations,
             long range)
         {
+            var response = new List<string[]>();
+            
             var matrix = await client.Matrix.GetMatrix(locations, range);
 
             var hamilton = new Core.Libs.Integration.Hamilton(locations.Count + 1);
 
-            return hamilton.Check(matrix, locations.Count);
+            var result = hamilton.Check(matrix, locations.Count);
+
+            for (int i = 0; i < result.Count; i++)              
+                response.Add(result[i].Select(a => locations[a]).ToArray());
+
+            return response;
         }
     }
 }
