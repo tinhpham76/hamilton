e05fa21a1987c824389807ddc7def36bd04c6f4b
tinhpham7676 <tinh_pham@outlook.com>
2021-06-05 09:25:11 +0700

    up key

--- src/frontend/src/clientapp/src/environments/environment.prod.ts
+++ src/frontend/src/clientapp/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  GOOGLE_MAP_API_KEY: 'AIzaSyBCjpw_s1yMau_HJkF_qRcp7SZbXLjyKzE'
+  GOOGLE_MAP_API_KEY: 'AIzaSyCXvhOuhC35mtiE9x6ZMTwUwu30j2ecVkI'
 };
